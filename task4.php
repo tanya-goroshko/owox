22575b11077027238ffa03dcc3c749b97ac5ccb3
Tanya Goroshko <tanyagoroshko@Tanyas-MacBook-Pro.local>
2019-01-24 03:00:57 +0200

    task4 added

---
+++ task4.php
@@ -0,0 +1,89 @@
+<?php
+    $order_data = array();
+
+    array_push($order_data, new Order(1, time(), 'ip'));
+
+    new makeOrder($order_data);
+
+    class makeOrder
+    {
+        public $order_data;
+        private $query = "START TRANSACTION; INSERT INTO orders(id, client_id, created, ip) VALUES ";
+        private $servername = 'localhost';
+        private $username = "username";
+        private $password = "password";
+
+        public function __construct($data)
+        {
+            $this->order_data = $data;
+            $this->makeQuery();
+        }
+
+        private function makeQuery()
+        {
+            $comma = "";
+            foreach ($this->order_data as $data)
+            {
+                $this->query .= $comma . "(" . $data->client_id . ", " . $data->created . ", '" . $data->ip . "')";
+                $comma = ", ";
+            }
+
+            $this->query .= "; INSERT INTO order_item(order_id, product_id, amount)".
+                "VALUES ";
+
+            $comma = "";
+            foreach ($this->order_data as $data)
+            {
+                foreach ($data->products as $prod)
+                {
+                    $this->query .= $comma . "(" . $prod->order_id . ", " . $prod->product_id . ", " . $prod->amount . ")";
+                    $comma = ", ";
+                }
+            }
+
+            $this->query .= "; COMMIT;";
+
+            $mysqli = new mysqli($this->servername, $this->username, $this->password);
+
+            if ($mysqli->connect_errno)
+                die("Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+
+            if (!$mysqli->multi_query($this->query)) {
+                echo "Не удалось выполнить мультизапрос: (" . $mysqli->errno . ") " . $mysqli->error;
+            }
+        }
+    }
+
+    class Order
+    {
+        public $client_id;
+        public $created;
+        public $ip;
+        public $products = [];
+
+        public function __construct($id, $when_created, $ip)
+        {
+            $this->client_id = $id;
+            $this->created = $when_created;
+            $this->ip = $ip;
+
+            for($j = 1; $j < 6; $j++)
+            {
+                array_push($this->products, new Product($j, $id, rand(1, 10)));
+            }
+        }
+    }
+
+    class Product
+    {
+        public $order_id;
+        public $product_id;
+        public $amount;
+
+        public function __construct($id, $order_id, $amnt)
+        {
+            $this->product_id = $id;
+            $this->order_id = $order_id;
+            $this->amount = $amnt;
+        }
+    }
--- task4.sql
+++ task4.sql
@@ -1,50 +1,77 @@
 CREATE TABLE IF NOT EXISTS `products` (
-    `id` INT(11) NOT NULL UNIQUE,
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(128) NOT NULL,
     `description` TEXT NOT NULL,
     `price` decimal(10, 2) unsigned DEFAULT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = MyISAM;
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `clients` (
-    `id` INT(11) NOT NULL UNIQUE,
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(128) NOT NULL,
     `email` VARCHAR(64) NOT NULL,
     `phone` VARCHAR(12) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = MyISAM;
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `orders` (
-    `id` SMALLINT(11) NOT NULL UNIQUE,
+    `id` SMALLINT(11) NOT NULL AUTO_INCREMENT,
     `client_id` INT(11) NOT NULL,
     `created` DATETIME NOT NULL,
     `ip` VARCHAR(15) NOT NULL,
     PRIMARY KEY (`id`),
-    INDEX `foreign_key_orders_index_1` (`client_id` ASC),
-    CONSTRAINT `foreign_key_orders_1`
+    INDEX `foreign_key_client_id` (`client_id` ASC),
+    CONSTRAINT `foreign_key_client_id`
     FOREIGN KEY (`client_id` )
-    REFERENCES `clients` (`id`)
+    REFERENCES `clients`(`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-) ENGINE = MyISAM;
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `order_item` (
-  `id` INT(11) NOT NULL UNIQUE,
-  `order_id` INT(11) NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `order_id` SMALLINT(11) NOT NULL,
   `product_id` INT(11) NOT NULL,
   `amount` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `foreign_key_order_item_index_1` (`order_id` ASC),
-  INDEX `foreign_key_order_item_index_2` (`product_id` ASC),
-  CONSTRAINT `foreign_key_order_item_1`
+  INDEX `foreign_key_order_id_index` (`order_id` ASC),
+  INDEX `foreign_key_product_id_index` (`product_id` ASC),
+  CONSTRAINT `foreign_key_order_id`
     FOREIGN KEY (`order_id` )
-    REFERENCES `orders` (`id` )
+    REFERENCES `orders`(`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `foreign_key_order_item_2`
+  CONSTRAINT `foreign_key_product_id`
     FOREIGN KEY (`product_id` )
-    REFERENCES `products` (`id` )
+    REFERENCES `products`(`id` )
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION
+)ENGINE = InnoDB;
 
+-- make order through transaction
+-- see task4.php file
+
+--select orders with specific products
+SELECT `order_item`.`order_id` FROM `order_item`
+        INNER JOIN `products` ON `products`.`id` = `order_item`.`product_id`
+WHERE `products`.`id` = YOUR_PRODUCT_ID GROUP BY `order_item`.`order_id`;
+
+--select orders of the specific client
+SELECT `orders`.`id` FROM `orders`
+        INNER JOIN `clients` ON `clients`.`id` = `orders`.`client_id`
+WHERE `clients`.`id` = YOUR_CLIENT_ID GROUP BY `orders`.`id`;
+
+--select 10 orders
+SELECT `orders`.`id`, `orders`.`created`, `over_one`.`count_product`, `over_one`.`avg_product_price` FROM `orders`
+        INNER JOIN (
+                SELECT `order_item`.`order_id` `id`, sum(`order_item`.`amount`) AS `count_product`, count(*) AS `cnt`, `average`.`avg_product_price` FROM `order_item`
+                    INNER JOIN (
+                          SELECT `id`, `price` FROM `products`
+                    ) `price` ON `price`.`id` = `order_item`.`product_id`
+                    INNER JOIN (
+                        SELECT `order_id`, avg(`pr`.`price` * `amount`) AS `avg_product_price` FROM `order_item`
+                            INNER JOIN(SELECT `id`, `price` FROM `products`) `pr` ON `pr`.`id` = `order_item`.`product_id` GROUP BY `order_item`.`order_id`
+                    ) `average` ON `average`.`order_id` = `order_item`.`order_id`
+                GROUP BY `order_item`.`order_id` HAVING `cnt` > 1
+        ) `over_one` ON `over_one`.`id` = `orders`.`id`
+ORDER BY `orders`.`created` limit 10;
